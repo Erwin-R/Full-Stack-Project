29b897cff3c97775a422cc596c32056fc96795bb
potatochip <rosaleserwin3@gmail.com>
2024-04-16 14:56:57 -0500

    check

--- src/main/java/com/potatochip/Main.java
+++ src/main/java/com/potatochip/Main.java
@@ -22,7 +22,7 @@ public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
-
+    // checking
     @Bean
     CommandLineRunner commandLineRunner(CustomerRepository customerRepository){
         return args -> {
