fac733066a6451bc9e0609d29e2ac216fa56b12a
potatochip <rerwin684@gmail.com>
2024-04-18 15:00:36 -0500

    changed git ignore file and added backend folder

--- backend/src/main/java/com/potatochip/Main.java
+++ backend/src/main/java/com/potatochip/Main.java
@@ -22,7 +22,7 @@ public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
-    // checking 2
+
     @Bean
     CommandLineRunner commandLineRunner(CustomerRepository customerRepository){
         return args -> {
