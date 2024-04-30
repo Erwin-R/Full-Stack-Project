fe3b1c7bddde26e9e850b2827cea2b562b721285
potatochip <rerwin684@gmail.com>
2024-04-30 16:06:07 -0500

    add counter to PingPongController

--- backend/src/main/java/com/potatochip/PingPongController.java
+++ backend/src/main/java/com/potatochip/PingPongController.java
@@ -5,10 +5,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PingPongController {
+    private static int COUNTER = 0;
     record PingPong(String result){}
 
     @GetMapping("/ping")
     public PingPong getPingPong(){
-        return new PingPong("pong");
+        return new PingPong("Pong: %s".formatted(++COUNTER));
     }
 }
