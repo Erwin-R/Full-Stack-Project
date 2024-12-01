62ca8d02ef7c9560f7929dec12964ca370e3a6d6
potatochip <rerwin684@gmail.com>
2024-12-01 12:04:19 -0600

    New React Routes

--- frontend/react/src/main.jsx
+++ frontend/react/src/main.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import Customer from './Customer.jsx'
 import { ChakraProvider } from '@chakra-ui/react'
 import { createStandaloneToast } from '@chakra-ui/react'
+import {Text } from '@chakra-ui/react'
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import './index.css'
 import Login from "./components/login/Login.jsx";
 import AuthProvider from "./components/context/AuthContext.jsx";
 import ProtectedRoute from "./components/shared/ProtectedRoute.jsx";
 import Signup from "./components/signup/Signup.jsx";
+import Home from "./Home.jsx";
 
 const { ToastContainer } = createStandaloneToast()
 
@@ -23,7 +25,11 @@ const router = createBrowserRouter([
     },
     {
         path: "dashboard",
-        element: <ProtectedRoute> <App/> </ProtectedRoute>
+        element: <ProtectedRoute><Home/></ProtectedRoute>
+    },
+    {
+        path: "dashboard/customers",
+        element: <ProtectedRoute> <Customer/> </ProtectedRoute>
     }
 ])
 
